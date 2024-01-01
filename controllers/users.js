c5e7d8577e389f8c5da8b3f40a1831787d97de0b
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-01-01 18:07:42 +0530

    feat: updated

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,46 @@
 const Users = require('../models/users.js');
 
-//get all users
+// Get all users
 exports.getUsers = (req, res, next) => {
-    Users.find({}).then((data) => {
-        res.send(data);
-    }).catch(next);
-}
+    Users.find({})
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(next);
+};
+
+// Get user by ID
+exports.getUserById = (req, res, next) => {
+    Users.findOne({ _id: req.params.id })
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(next);
+};
+
+// Create a new user
+exports.createUser = (req, res, next) => {
+    Users.create(req.body)
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(next);
+};
+
+// Update user by ID
+exports.updateUserById = (req, res, next) => {
+    Users.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(next);
+};
+
+// Delete user by ID
+exports.deleteUserById = (req, res, next) => {
+    Users.findOneAndDelete({ _id: req.params.id })
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(next);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,12 @@
 const express = require('express');
-const Users = require('../models/users.js');
 const router = express.Router();
+const usersController = require('../controllers/users');
 
-const controller = require('../controllers/users');
-router.get('/', controller.getUsers);
-// get users request
-router.get('/users',(req,res,next) => {
-    Users.find({}).then((data) =>{
-        res.send(data);
-    }).catch(next);
-});
-router.get('/users', (req, res) => {
-    Users.find((err, doc) => {
-        if (err) {
-            console.log('Error While Getting' + err);
-        }
-        else {
-            res.send(doc);
-        }
-    })
-});
-
-// get users by id request
-router.get('/users/:id', (req, res, next) => {
-    Users.findOne({ _id: req.params.id }).then((data) => {
-        res.send(data);
-    });
-});
-
-// post users request
-router.post('/users',(req,res,next) => {
-    Users.create(req.body).then((data) =>{
-        res.send(data);
-    }).catch(next);
-});
-
-// update users by id request
-router.put('/users/:id', (req, res, next) => {
-    Users.findOneAndUpdate({ _id: req.params.id }, req.body).then((data) => {
-        Users.findOne({ _id: req.params.id }).then((data) => {
-            res.send(data);
-        });
-    });
-});
-
-// delete users by id request
-router.delete('/users/:id', (req, res, next) => {
-    Users.findOneAndDelete({ _id: req.params.id }).then((data) => {
-        res.send(data);
-    });
-});
+// Routes
+router.get('/users', usersController.getUsers);
+router.get('/users/:id', usersController.getUserById);
+router.post('/users', usersController.createUser);
+router.put('/users/:id', usersController.updateUserById);
+router.delete('/users/:id', usersController.deleteUserById);
 
 module.exports = router;
